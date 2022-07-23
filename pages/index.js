a17c5b3b8bdffbe4c7ccdef41b89b1e9a491f19c
arie <arieinjector@gmail.com>
2022-07-23 20:46:37 +0700

    add menu sidebar

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export function getServerSideProps(ctx){
 
 const Home = ({dataToken}) => {
   const [isOpenChat , setIsOpenChat] = useState(false)
+  const [isOpenMenu , setIsOpenMenu] = useState(false)
 
   const scrollmessage = useRef(null)
   
@@ -35,10 +36,10 @@ const Home = ({dataToken}) => {
 
       <div className="w-screen lg:w-[98vw] xl:w-[1260px] 2xl:w-[1500px] h-screen lg:h-[90vh] bg-[#7C83FD] flex">
 
-        <aside className="w-max lg:w-[300px] xl:w-[340px] 2xl:w-[400px] h-screen lg:h-[90vh] px-5 bg-[#533E85]">
+        <aside className={isOpenMenu ? 'w-[250px] lg:w-[300px] xl:w-[340px] 2xl:w-[400px] h-screen lg:h-[90vh] px-5 bg-[#533E85] relative transition-all' : 'w-max lg:w-[300px] xl:w-[340px] 2xl:w-[400px] h-screen lg:h-[90vh] px-5 bg-[#533E85] relative transition-all'}>
 
-          <div className="flex items-center lg:flex-row flex-col py-3 lg:mb-0 mb-2">
-            <img src="/icon/menu.png" className="lg:mr-5 lg:mb-0 mb-5" alt="" />
+          <div className={isOpenMenu ? 'flex lg:items-center items-start lg:flex-row flex-col py-3 lg:mb-0 mb-2' : 'flex items-center lg:flex-row flex-col py-3 lg:mb-0 mb-2'}>
+            <img src={isOpenMenu ? 'icon/silang.png' : 'icon/menu.png'} onClick={() => setIsOpenMenu(!isOpenMenu)} className="lg:mr-5 lg:mb-0 mb-5 cursor-pointer z-10" alt="" />
             <div className="flex items-center bg-[#F5F5F5] py-3 lg:py-2 px-3 rounded-full grow">
               <img src="/icon/search.png" className="lg:mr-4" alt="" />
               <input type="text" placeholder="Search" className="bg-transparent focus:outline-none hidden lg:block" />
@@ -73,6 +74,17 @@ const Home = ({dataToken}) => {
             <CardUser setIsOpenChat={setIsOpenChat} onBottomScroll={onBottomScroll} />
             <CardUser setIsOpenChat={setIsOpenChat} onBottomScroll={onBottomScroll} />
           </div>
+
+          <div className={isOpenMenu ? 'absolute top-0 left-0 w-full h-full bg-[#533E85] transition-all duration-300 overflow-hidden pt-16 px-5 text-white' : 'absolute top-0 left-0 w-0 h-full bg-[#533E85] transition-all overflow-hidden pt-16'}>
+              <div className="flex items-center mb-7">
+                <img src={dataToken.imgprofile} className="w-[55px] rounded-full mr-4" alt="" />
+                <p className="text-xl font-medium">{dataToken.username}</p>
+              </div>
+
+              <p className="p-3 bg-[#7C83FD] rounded-lg font-medium mb-4">Theme</p>
+              <p className="p-3 bg-[#7C83FD] rounded-lg font-medium">Sign Out</p>
+          </div>
+
         </aside>
 
         <div className="lg:h-[90vh] bg-[url('/bgchat.png')] bg-cover bg-center grow">
